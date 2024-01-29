c19840057cb1e2295c53f59bfc218f4097418d9b
Gustavo Maxwel <maxwelgustavo@gmail.com>
2024-01-29 14:41:59 -0300

    Enquanto..

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,21 +1,22 @@
 alert('Boas vindas ao jogo do número secreto.');
-
-// Adicione um console.log para verificar o valor de "chute" após a entrada do usuário
-let chute = prompt('Escolha um número entre 1 e 10');
-console.log('Valor do chute:', chute);
-
-let numeroSecreto = 2;
+let numeroSecreto = 5;
 console.log(numeroSecreto);
+let chute
 
-// Adicione um console.log para verificar a comparação entre "chute" e "numeroSecreto"
-console.log('Resultado da comparação:', chute == numeroSecreto);
+// enquanto chute não for igual ao n.s.
+while (chute != numeroSecreto) {
+    chute = prompt('Escolha um número entre 1 e 10');
+    // se chute for igual ao número secreto
+    if (chute == numeroSecreto) {
+        // Template Strings
+        alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto}`);
+        break;
+    } else {
+        if (chute > numeroSecreto) {
+            alert(`O número secreto é menor que ${chute}`);
+        } else {
+            alert(`O número secreto é maior que ${chute}`);
+        }
+    }
 
-// se chute for igual ao número secreto
-if (chute == numeroSecreto) {
-  // Template Strings
-  alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto}`);
-} else {
-  // Adicione um console.log para verificar o valor de "numeroSecreto" quando o jogador erra
-  console.log('Valor do número secreto:', numeroSecreto);
-  alert('O número secreto era ' + numeroSecreto);
 }
